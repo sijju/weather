f5e0b1f5e95a4a6ffed59709ccdf5d98f5ab0881
KarthikMutyala1234 <132066861+KarthikMutyala1234@users.noreply.github.com>
2023-05-06 10:22:04 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+  return (
+   <>
+    <div className='container'>
+      <div className="search">
+
+      </div>
+      <div className="details">
+        <div className="searchData">
+
+        </div>
+        <div className="currentData">
+          
+        </div>
+      </div>
+    </div>
+   </>
+  )
+}
+
+export default App
