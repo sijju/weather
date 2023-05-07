5eb45e89360cda6b2b031e75e8770e2207ec0172
KarthikMutyala1234 <132066861+KarthikMutyala1234@users.noreply.github.com>
2023-05-07 17:43:58 +0530

    removed router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,28 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Error from './components/Error.jsx'
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element : <App />,
-    errorElement : <Error />,
-    children : [
-      {
-        path:'/search',
-        element : <App />
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
 
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>,
 
 )
